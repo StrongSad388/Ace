1944f7f140182815de4fa2ad0348c891e3743e6b
Shaheen Ali <shaheen.ali@lumen.com>
2024-01-19 20:40:55 -0500

    Create a ship subclass

--- public/AceShips.js
+++ public/AceShips.js
@@ -1,1 +1,11 @@
-console.log("Here I am!");
+class AceShip extends PIXI.Graphics {
+    constructor(name, shipClass, x = 0, y = 0, color, visible = true) {
+        super();
+        this.name = name;
+        this.shipClass = shipClass;
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.visible = visible;
+    }
+}
