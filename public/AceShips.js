dc23fcb9878bd083660182af7ea9646f6cfe9f95
Shaheen Ali <ali@smallmoon.com>
2024-01-21 14:59:09 -0500

    Add a mine

--- public/AceShips.js
+++ public/AceShips.js
@@ -9,24 +9,24 @@ class AceShip extends PIXI.Container {
         this.color = color;
         this.visible = visible;
         const graphic = new PIXI.Graphics();
-   
+
         this.graphic = graphic;
         this.addChild(this.graphic);
         if (this.shipClass === 'frigate') {
             this.drawFrigate();
         } else if (this.shipClass === 'battleship') {
             this.drawBattleShip();
+        } else if (this.shipClass === 'mine') {
+            this.drawMine();
         }
-        this.nameGraphic = new PIXI.Text(this.name, {fontFamily: 'Arial', fontSize: 6, fill: 'black', align: 'center'});
+        this.nameGraphic = new PIXI.Text(this.name, { fontFamily: 'Arial', fontSize: 6, fill: 'black', align: 'center' });
         graphic.addChild(this.nameGraphic);
     }
+    move(vX = 0, vY = 0) {
+        this.x += this.vx + vX;
+        this.y += this.vy + vY;
+    }
     drawFrigate() {
-        // Triangle
-        //const path = [
-        //    this.x - 8, this.y + 10,
-        //    this.x + 8, this.y + 10,
-        //    this.x, this.y
-        //];
         const path = [
             this.x, this.y,
             this.x + 10, this.y,
@@ -43,8 +43,8 @@ class AceShip extends PIXI.Container {
         graphic.drawPolygon(path);
         graphic.position.set(this.x, this.y);
         graphic.endFill();
-   }
-   drawBattleShip() {
+    }
+    drawBattleShip() {
         const path = [
             this.x, this.y,
             this.x + 25, this.y,
@@ -53,18 +53,29 @@ class AceShip extends PIXI.Container {
             this.x, this.y - 10,
             this.x - 6, this.y - 5
         ]
-       const graphic = this.graphic;
-       graphic.clear();
-       graphic.lineStyle(1, this.color);
-       graphic.beginFill(this.color);
-       graphic.drawPolygon(path);
-       //graphic.drawRect(this.x, this.y, 25, 10);
-       graphic.endFill();
-   }
-   move(vX = 0, vY = 0) {
-       this.x += this.vx + vX;
-       this.y += this.vy + vY;
-   }
+        const graphic = this.graphic;
+        graphic.clear();
+        graphic.lineStyle(1, this.color);
+        graphic.beginFill(this.color);
+        graphic.drawPolygon(path);
+        graphic.position.set(this.x, this.y);
+        graphic.endFill();
+    }
+
+    drawMine() {
+        const path = [
+            this.x - 8, this.y + 10,
+            this.x + 8, this.y + 10,
+            this.x, this.y
+        ];
+        const graphic = this.graphic;
+        graphic.clear();
+        graphic.lineStyle(1, this.color);
+        graphic.beginFill(this.color);
+        graphic.drawPolygon(path);
+        graphic.position.set(this.x, this.y);
+        graphic.endFill();
+    }
 }
 
 class AceManager {
